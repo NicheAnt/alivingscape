589cd905a3cb076c7a907fbac6d4adbd5999e2b3
NicheAnt <nishant.sth@gmail.com>
2019-06-20 17:17:43 +0530

    Add files via upload

---
+++ application.js
@@ -0,0 +1,12 @@
+
+//loading sequence for home-page: after everything loads
+window.onload = function() {
+  //$('.logo').fadeIn(1000);
+  
+};
+
+//enabling js after page loads
+$(document).ready(function() {
+
+
+});//full dom code
